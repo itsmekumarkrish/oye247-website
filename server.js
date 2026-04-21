df11c53b83c1ec06cdbdaf3f2d2cf49763eca63a
SHIV <shiv@SHIVs-MacBook-Pro.local>
2026-04-21 08:28:05 +0545

    fixed railway binding

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
